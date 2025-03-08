7a2e49a67b05ddf74ad1cc46604f1bf4977d2df4
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-08 07:42:04 -0800

    In intranet we have to use or gate

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for i in range(0, 100):
         i += 1
-        if i % 3 == 0 and i % 5 == 0:
+        if i % 3 == 0 or i % 5 == 0:
             i = "Fizzbuzz"
 
         print("{}".format(i), end=" ")
