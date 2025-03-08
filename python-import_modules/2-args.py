b5ee9a1be8a54670f067abbb1aac4d312610e219
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-08 12:30:31 -0800

    Modification

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -12,6 +12,5 @@ if __name__ == "__main__":
         else:
             print(f"{arg_len} argument:")
 
-        for i, arg in enumerate(argv, 1)
-            print(f"{i}: {argv}")
-
+        for i, arg in enumerate(argv, 1):
+            print(f"{i}: {arg}")
