55205657c5d53480b80f052bb39f51c7021be065
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-08 12:38:18 -0800

    Updating args function

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,15 +2,15 @@
 import sys
 
 if __name__ == "__main__":
-    argv = sys.arg[1:]
-    arg_len = len(argv)
-    for arg in argv:
-        if arg_len == 0:
+    arg = sys.argv[1:]
+    arg_len = len(arg)
+    for argu in arg_len:
+        if argu == 0:
             print("0 arguments.")
-        elif arg_len > 1:
+        elif argu > 1:
             print(f"{arg_len} arguments:")
         else:
             print(f"{arg_len} argument:")
 
-        for i, arg in enumerate(argv, 1):
-            print(f"{i}: {arg}")
+        for i, argum in enumerate(arg, 1):
+            print(f"{i}: {argum}")
