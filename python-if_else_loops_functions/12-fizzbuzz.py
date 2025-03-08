907fab50893ccaefae5e95f48bba58106d3208c1
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-08 07:39:46 -0800

    Task 12 added

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(0, 100):
+        i += 1
+        if i % 3 == 0 and i % 5 == 0:
+            i = "Fizzbuzz"
+
+        print("{}".format(i), end=" ")
