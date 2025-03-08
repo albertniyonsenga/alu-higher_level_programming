24230cb71aef142d6b2191ef49bc63b366b29927
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-08 05:06:07 -0800

    PEP8 will kill me

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,8 @@ number = random.randint(-10000, 10000)
 digit = abs(number) % 10
 if number < 0:
     digit = -digit
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, digit))
+    print("Last digit of {} is {} and is ".format(number, digit), end="")
+    print("less than 6 and not 0")
 
 if number >= 0:
     print("Last digit of {} is {} and is ".format(number, digit), end="")
