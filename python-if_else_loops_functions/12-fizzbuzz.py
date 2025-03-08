3e5d57a8c95152f77c8ae483bf9a6ee3cbe24ae7
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-08 07:49:08 -0800

    Capitalizng buzz in And

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for i in range(0, 100):
         i += 1
         if i % 3 == 0 and i % 5 == 0:
-            i = "Fizzbuzz"
+            i = "FizzBuzz"
         elif i % 3 == 0:
             i = "Fizz"
         elif i % 5 == 0:
