ef8bd210c2634afb6365f5ff01b417c99c7033ba
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-08 12:47:01 -0800

    Removing wrong for loop

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,13 +4,13 @@ import sys
 if __name__ == "__main__":
     arg = sys.argv[1:]
     arg_len = len(arg)
-    for argu in range(0, len(arg_len):
-        if argu == 0:
-            print("0 arguments.")
-        elif argu > 1:
-            print(f"{arg_len} arguments:")
-        else:
-            print(f"{arg_len} argument:")
 
-        for i, argum in enumerate(arg, 1):
-            print(f"{i}: {argum}")
+    if argu_len == 0:
+        print("0 arguments.")
+    elif argu_len > 1:
+        print(f"{arg_len} arguments:")
+    else:
+        print(f"{arg_len} argument:")
+
+    for i, argum in enumerate(arg, 1):
+        print(f"{i}: {argum}")
