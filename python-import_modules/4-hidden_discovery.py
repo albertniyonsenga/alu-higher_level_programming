8545fe0cf4c9c8bf7ba1a41b621be889e242adf3
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-08 23:20:08 -0800

    Clarifying the attributes

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,7 @@ import hidden_4
 def hidden():
     names = dir(hidden_4)
     for name in names:
-        if not name.startwith("__"):
+        if name[2:] != "__":
             print(name)
 
 if __name__ == "__main__":
