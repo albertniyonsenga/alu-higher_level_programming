b1fec4022e85bdaecab900b71795080d6aa82fa9
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-08 22:55:33 -0800

    Embarcing the pain that int is not iterable

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,6 +2,5 @@
 import sys
 
 if __name__ == "__main__":
-    for arg in sys.argv[1:]:
-        tot = sum(int(arg))
-        print(tot)
+    tot = sum(int(arg) for arg in sys.argv[1:])
+    print(tot)
