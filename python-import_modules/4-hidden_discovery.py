6a1c5403fc067bda041f09b656fb022927ea6985
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-08 23:24:09 -0800

    Indentation handling and where to look for start of underscores

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import hidden_4
 
+
 def hidden():
     names = dir(hidden_4)
     for name in names:
-        if name[2:] != "__":
-            print(name)
+        if name[:2] != "__":
+            print(f"{name}")
+
 
 if __name__ == "__main__":
     hidden()
