473487a66512f3e62b62fc87806eafa0daaad118
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-11 03:29:41 -0700

    Making sure for empty logi will work

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        sentence = 0
-    return tuple((len(sentence), None if not sentence else sentence[:1]))
+    if sentence:
+        len = len(sentence)
+    else:
+        len = 0
+    return tuple((len, None if not sentence else sentence[:1]))
