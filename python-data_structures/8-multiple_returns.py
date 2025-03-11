88753806afba6a408062c68e26ad167541f49b90
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-11 03:25:23 -0700

    Removing last arg

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,4 +2,4 @@
 def multiple_returns(sentence):
     if not sentence:
         sentence = 0
-    return tuple(len(sentence), None if not sentence else sentence[:1])
+    return tuple((len(sentence), None if not sentence else sentence[:1]))
