4e196c925784bbad5408bbcee1ba3c1ad8fc08fe
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-11 04:56:25 -0700

    Lets cook :smile:

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if my_list:
-        elem = my_list[idx]
-        my_list.remove(elem)
-        return my_list
+        if idx in range(0, len(my_list)):
+            elem = my_list[idx]
+            my_list.remove(elem)
+            return my_list
+        else:
+            return my_list
     else:
         return my_list
