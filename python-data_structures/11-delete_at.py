8fc16b323977f6841dcc9106d49c2dac3c340390
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-11 04:46:11 -0700

    Removing iter

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    for idx in my_list:
-        elem = my_list[idx]
-        my_list.remove(elem)
-        return my_list
+    elem = my_list[idx]
+    my_list.remove(elem)
+    return my_list
