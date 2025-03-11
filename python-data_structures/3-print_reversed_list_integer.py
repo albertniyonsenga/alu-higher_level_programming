27aed3ab5b0da036deccda94d07e7f90eefb0f32
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-11 00:44:03 -0700

    Adding if to check if list has values

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list = my_list.reverse()
-    print("{:d}".format(my_list))
+    if my_list is None:
+        return ""
+    for i in my_list.reverse():
+        print("{:d}".format(i))
