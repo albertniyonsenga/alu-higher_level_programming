fadc34b76d66b1396ff20cbced9788513409609a
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-11 04:15:19 -0700

    Simple way

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        maxmum = my_list[-1]
-        for nums in my_list[:-1]:
-            if nums > maxmum:
-                maxmum = nums
-            return maxmum
-    elif len(my_list) == 1:
-        maxmum = my_list[0]
+        maxmum = sorted(my_list)[-1]
         return maxmum
     else:
         return None
