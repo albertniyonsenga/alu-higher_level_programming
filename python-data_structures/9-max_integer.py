6304608534a22d3ee547749099d7855f1302cc22
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-11 03:56:29 -0700

    Updating the logic

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maxmum = my_list[0]
-    for nums in my_list[1:]:
-        if nums > maxmum:
-            maxmum = nums
-            return maxmum
+    if my_list:
+        maxmum = my_list[-1]
+        for nums in my_list[:-1]:
+            if nums > maxmum:
+                maxmum = nums
+                return maxmum
+    elif len(my_list) = 1:
+        maxmum = my_list[0]
+        return maxmum
+    else:
+        return None
+
