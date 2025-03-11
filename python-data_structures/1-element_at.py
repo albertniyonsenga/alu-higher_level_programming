aa2595c0d94ea013774d66aff687f6a4e55a1f2c
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-10 23:59:35 -0700

    Updating the logic in fun

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
+def element_at(my_list[idx]):
     for idx in my_list:
         if idx < 0:
             return None
         elif idx > len(my_list):
             return None
         
-    print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+    print("Element at index {:d} is {}".format(idx, element_at(my_list[idx])))
