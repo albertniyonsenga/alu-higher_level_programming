0c196a3f09215de8cbc253e90f04715c5b1183b2
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-11 02:10:11 -0700

    Adding task6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        if not i:
+            print("")
+            for innum in i:
+                if innum == i[-1]:
+                    print("{:d}".format(innum))
+                else:
+                    print("{:d}".format=(innum), end="")
