76cf9bb3cde3e1badc9cd5bb82ea09bf28c7dcd7
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-11 00:15:06 -0700

    Update after understanding

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-def element_at(my_list[idx]):
-    for idx in my_list:
-        if idx < 0:
-            return None
-        elif idx > len(my_list):
-            return None
-        
-    print("Element at index {:d} is {}".format(idx, element_at(my_list[idx])))
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    if idx > len(my_list):
+        return None
+    
+    return my_list[idx]
