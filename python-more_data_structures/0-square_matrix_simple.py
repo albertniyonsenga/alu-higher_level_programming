043a4381c7517e4c4806a69ca926c111681143f6
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-19 10:24:19 -0700

    Task 1 added

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+!#!usr/bin/python3
+def sqauare_matrix_simple(matrix=[]):
+    if not matrix:
+        for row in matrix:
+            for element in row:
+                element = element * element
+                return element
+    return matrix
