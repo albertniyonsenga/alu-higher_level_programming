c638970731d04f3e30ef5255c9382ef4cbb8926f
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-21 03:39:19 -0700

    Revert "Adding res to handle empty lists"
    This reverts commit ffb46e26447ac3ff91694fdbb42cd2e402b3d9d2.

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     my_list = list(dict.fromkeys(my_list))
-    res = 0
     for ele in my_list:
-        res += ele
-        return res
+        return sum(my_list)
