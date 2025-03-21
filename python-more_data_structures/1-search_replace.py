baecba9c39a22a93baffa657a7b170306f9939df
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-21 02:24:47 -0700

    Using some of ex from TFM of python

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if not my_list:
-        print()
-        for ele in my_list:
-            if ele != search:
-                return ele
-            else:
-                return replace
-
-            return my_list
+    return [ele if ele != search else replace for ele in my_list]
