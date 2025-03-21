1c37545dc4608f98869cb03af06a464c6f968583
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-21 00:58:21 -0700

    Adding the Print method

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if not matrix:
+        print()
         for row in matrix:
             for element in row:
                 element = element * element
