ffb46e26447ac3ff91694fdbb42cd2e402b3d9d2
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-21 03:09:02 -0700

    Adding res to handle empty lists

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     my_list = list(dict.fromkeys(my_list))
+    res = 0
     for ele in my_list:
-        return sum(my_list)
+        res += ele
+        return res
