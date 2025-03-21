df7516867d0ea6b87f2889775dc6d63db9a56824
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-21 02:58:55 -0700

    Specifying that we got new list

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    for ele in list(dict.fromkeys(my_list)):
+    my_list = list(dict.fromkeys(my_list))
+    for ele in my_list:
         return sum(my_list)
