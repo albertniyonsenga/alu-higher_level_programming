bb883bec319f2e0512aa3587e81e907dad717a2c
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-21 02:09:56 -0700

    Modification

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for ele in my_list:
-        if ele != search:
-            return ele
-        else:
-            return replace
+    if not my_list:
+        print()
+        for ele in my_list:
+            if ele != search:
+                return ele
+            else:
+                return replace
     return my_list
