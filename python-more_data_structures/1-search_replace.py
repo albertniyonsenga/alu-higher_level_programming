7828c38753412d9a093005c82ebd8588d7046f84
albertniyonsenga <a.niyonseng@alustudent.com>
2025-03-21 01:44:14 -0700

    Task 1 added

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for ele in my_list:
+        if ele != search:
+            return ele
+        else:
+            return replace
