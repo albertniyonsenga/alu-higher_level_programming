acd2afc06121bf992be1d89a4162363fada2becf
Albert NIYONSENGA <a.niyonseng@alustudent.com>
2025-03-21 12:48:46 +0200

    Update 2-uniq_add.py
    Let's get the values of list into another to check

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    my_list = list(dict.fromkeys(my_list))
-    for ele in my_list:
-        return sum(my_list)
+    my = list(dict.fromkeys(my_list))
+    for ele in my:
+        return sum(my)
